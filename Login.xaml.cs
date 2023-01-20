5925621450840597d86bee31ba69cfbe225b63bf
TAJassson <despa.tse@gmail.com>
2023-01-20 16:07:37 +0800

    Add comment

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -4,6 +4,8 @@ using System.Security.Cryptography;
 using System.Diagnostics;
 using System;
 using System.Windows;
+using System.Data.SqlClient;
+using 
 using System.Text;
 
 namespace stock_manager_system
@@ -16,12 +18,26 @@ namespace stock_manager_system
         public Login()
         {
             InitializeComponent();
+
         }
 
+        public void sqlconnect()
+        {
 
-        // Password Protection
-        //SHA512
-        private void SHAEncrypt(string data)
+        }
+        
+        public void sqllogin()
+        {
+
+
+        }
+
+        public void connecttoserver()
+        {
+
+        }
+        //Password protection
+        private void SHA512Encrypt(string data)
         {
             using var sha512 = SHA512.Create();
             byte[] bytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(data));
@@ -32,9 +48,12 @@ namespace stock_manager_system
             }
             data = sb.ToString();
         }
+        //method: string message to using this void with
+        //string message = "123";
+        //SHA512Encrypt (message); //return result
 
-        //MD5, String Password and inputdata
-        private void btnMD5Decrypt_Click(string password, string inputdata)
+        //isn't support the bool or float, the data need using data.ToString(); send.
+        void MD5Decrypt(string password, string inputdata)
         {
             //Convert a string to byte array
             //  string inputdata;
@@ -43,15 +62,26 @@ namespace stock_manager_system
             {
                 byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));//Get hash key
                 //Decrypt data by hash key
+                /*
                 using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
                 {
                     ICryptoTransform transform = tripDes.CreateDecryptor();
-                    byte[] results = transform.TransformFinalBlock(inputdata, 0, inputdata.Length);
+                    byte[] results = transform.TransformFinalBlock(inputdata, 0, inputdata.Length); //?? idk what error on this :(
                     //    txtDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
                     string outputdata = Convert.ToBase64String(results);
                 }
+                */
             }
         }
-        // Password Protection
+        //method: using this void with string password and message 
+        //string message = "123";
+        //string password = "123";
+        //string result = MD5Decrypt (message, password).ToString();
+        //MessageBox.Show (result);
+        void MD5Encrypt(string password, string inputdata)
+        {
+
+        }
+        //
     }
 }
